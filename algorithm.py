f09ac58abbe921047a4395b27b0cc033efb2d931
LucienLuc <52895301+LucienLuc@users.noreply.github.com>
2020-03-28 00:38:11 -0700

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,30 @@
+
+
+'''
+STRINGS
+Soundex
+Levenstein distance
+Abrevations for States
+
+
+Account Number: check for name in account number (feel like this is useless)
+Names: abreviations, common shorter versions, soundex, levenstein, make all lowercase when checking
+        common longer versions (also try to deal with anne-marie vs anne), common other spellings, keyboard distance typos, characters that look similiar
+        switch first/last, special characters (T-J vs T.J vs TJ)
+Date of Birth: Levenstein, swapping month/day/year
+Sex: abbreviations
+Streets: possibly swapping of words, (do everything as names but for multiple words)
+Zip: Levenstein
+States: (same as names)
+
+General Notes:
+lonber the word/input = more weight in confidence (because more chance to get it right)
+a/o l/i m/n for physical writing forms
+levenshtein or mederau/levenshtein
+
+Street > State > City > Zip > Sex 
+
+How to sort/compare people:
+    add streets into a set (unique)
+    go through the set for that street to 
+'''
